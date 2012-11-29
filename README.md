0f26d1aa2e3b01ea2c25647f153714c7bc8afe50
Blai Pratdesaba <blai.pratdesaba@visualdna.com>
2012-11-29 17:52:15 +0000

    first upload and added README.md

--- README.md
+++ README.md
@@ -2,3 +2,33 @@ jquery.animateSprite
 ====================
 
 Animating sprites using jQuery made easy
+
+How to use it
+-------------
+
+1. Create an HTML Block Element (aka DIV) and define its width and height exactly as the with and height of the sprite. 
+
+2. Put the sprite file as a background image. 
+
+3. Call the plugin. Example:
+
+```javascript 
+	$(".sprite").animateSprite({
+		columns: 10,
+		totalFrames: 20,
+		duration: 2000, 
+		loop: true,
+		complete: function(){
+			alert("Sprite animation complete!");
+		}
+	})
+```	
+
+##### Options
+```javascript
+	columns: 		"int" the number of columns the sprite sheet has, default 10
+	totalFrames:	"int" number of frames the animation has
+	duration:		"int" time to complete the animation, in milliseconds
+	complete:		"function" called after the animation has finished (not called if is loop)
+	loop:			"bool" if the animation has to loop
+```
---
+++ jquery.animateSprite.js
@@ -0,0 +1,84 @@
+$(function(){
+
+  $.fn.animateSprite = function(parameters){
+    
+    // @author: Blai Pratdesaba <blai.pratdesaba@visualdna.com>
+
+    // parameters:
+    // target, name of the target (p. ex.: '.cord1')
+    // columns, number of columns of the sprite sheet
+    // totalFrames, number of frames of the animation
+    // duration, miliseconds
+    // complete, callback called when the function is completed
+
+    // TODOS:
+    // - The plugin doesn't chain correctly $
+    // - This only works with the first element
+
+    //init values
+    var animation = {
+      frame: 1,
+      currentFrame: 1
+    };
+
+
+    parameters = (parameters !== undefined ) ? parameters : {};
+    animation.target = $(this[0]);
+    animation.width = (animation.target !== undefined ) ? $(animation.target).width() : 0;
+    animation.height = (animation.target !== undefined ) ? $(animation.target).height() : 0;
+    animation.totalFrames = (parameters.totalFrames !== undefined ) ? parameters.totalFrames : 1;
+    animation.columns = (parameters.columns !== undefined ) ? parameters.columns : 10;
+    animation.duration = (parameters.duration !== undefined ) ? parameters.duration : 2000;
+    animation.complete = (parameters.complete !== undefined && typeof(parameters.complete) === "function") ? parameters.complete : function(){};
+    animation.loop = (parameters.loop !== undefined ) ? parameters.loop : false;
+
+    animation.updateAnimation = function(ev){
+
+      // executing the code only when needed
+      if ( this.currentFrame !== Math.ceil(ev)){
+
+        var row, column;
+        this.currentFrame = Math.ceil(ev);
+
+        row = Math.floor(this.currentFrame / this.columns);
+        column = this.currentFrame % this.columns;
+
+        //changing background
+        this.target.css("background-position", (-animation.width * column) +'px '+  (-animation.height * row) + 'px');
+      }
+    };
+
+    if(animation.loop === true){ 
+
+
+
+    }
+
+    var fAnimation = function(){
+      $(animation).animate({
+        frame: animation.totalFrames
+      }, {
+        duration: animation.duration,
+        easing: 'linear',
+        step: animation.updateAnimation,
+        complete: function(e){
+          animation.updateAnimation(e)
+          if (animation.loop === true) {
+          
+            this.frame = 0;
+            fAnimation()
+          } else  {
+
+            animation.complete()
+          }
+        }
+      });
+    }
+    fAnimation();
+
+
+    
+    return this;
+  };
+
+});
